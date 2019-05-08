11123702fa8bd28a526f65a8bdeac9197f966841
laytan <Laytanlaats@hotmail.com>
2019-05-08 19:05:56 +0200

    updated plugin description

--- npc.php
+++ npc.php
@@ -8,8 +8,9 @@
  *
  * @wordpress-plugin
  * Plugin Name: NPC
- * Plugin URI:  https://www.npc.com/
- * Description: A plugin containing blocks specific to the company NPC
+ * Plugin URI:  https://github.com/laytan/full-width-text-and-button-overlay-cards
+ * Description: A plugin for the gutenberg editor utilizing the new blocks to make a block that's full width with a
+ * background, overlay, header, buttons and some text.
  * Version:     1.0
  * Author:      Laytan Laats
  * Author URI:  https://www.github.com/laytan
@@ -19,19 +20,21 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
- //Define a namespace so wordpress loads our plugin correctly
+//Define a namespace so wordpress loads our plugin correctly
 namespace npc\npc;
 
-//  Exit if accessed directly.
-defined('ABSPATH') || exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
 /**
  * Gets this plugin's absolute directory path.
  *
  * @return string
  */
-function _get_plugin_directory() {
-	return __DIR__;
+function _get_plugin_directory()
+{
+    return __DIR__;
 }
 
 /**
@@ -39,16 +42,17 @@ function _get_plugin_directory() {
  *
  * @return string
  */
-function _get_plugin_url() {
-	//Define as static because this will never change between calls
-	static $plugin_url;
+function _get_plugin_url()
+{
+    //Define as static because this will never change between calls
+    static $plugin_url;
 
-	//If the static variable has not been set this reqeuest, define $plugin_url
-	if ( empty( $plugin_url ) ) {
-		$plugin_url = plugins_url( null, __FILE__ );
-	}
+    //If the static variable has not been set this reqeuest, define $plugin_url
+    if (empty($plugin_url)) {
+        $plugin_url = plugins_url(null, __FILE__);
+    }
 
-	return $plugin_url;
+    return $plugin_url;
 }
 
 // Include the file that enqueues our other files
